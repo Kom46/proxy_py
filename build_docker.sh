92b8a5d9bfb8fe3b1ab137cee3786ab5f4fecdbe
Kom46 <kommunizm46@gmail.com>
2024-04-23 12:02:41 +0300

    changed basheng

--- build_docker.sh
+++ build_docker.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 if [ $# -eq 0 ]
 then
 	tag='latest'
@@ -8,5 +8,5 @@ else
 	git checkout $tag || exit 1
 fi
 
-docker build -t proxy_py:$tag .
+docker build -t kom46/proxy_py:$tag .
 docker push kom46/proxy_py:$tag
